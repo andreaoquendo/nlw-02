4eb5927b0df6c3b7c992e4ba778d328905fbf048
Andrea <oquendo.3455@gmail.com>
2021-12-20 09:53:54 -0300

    landing page's style done

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -7,11 +7,13 @@ import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
+import './styles.css';
+
 // Parei em 1:09:35
 
 export default function Landing () {
     return (
-        <div className="page-landing">
+        <div id="page-landing">
             <div id="page-landing-content" className="container">
                 <div className="logo-container">
                     <img src={logoImg} alt="Proffy" />
